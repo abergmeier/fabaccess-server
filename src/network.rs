9e7b18e7421ecd397a2d7b390fbeaefb22814606
Gregor Reitzenstein <me@dequbed.space>
2020-09-15 17:17:00 +0200

    Writing down ideas for event networks and their handling

---
+++ src/network.rs
@@ -0,0 +1,25 @@
+use futures_signals::signal::Signal;
+
+struct Network {
+
+}
+
+impl Network {
+    pub fn new() -> Self {
+        Self {  }
+    }
+
+    /// react to a signal coming in by running a future with $parameter
+    // TODO: Actually take a parameter.
+    pub fn react<S: Signal, F: Fn() -> ()>(&mut self, s: S, f: F) {
+        unimplemented!()
+    }
+
+    /// Filter an incoming signal
+    ///
+    /// Idea being that bffh builds an event network that filters an incoming event into an
+    /// the appropiate (sub)set of signal handlers based on pretty dynamic configuration.
+    pub fn filter<S: Signal, F: Fn() -> bool>(&mut self) {
+        unimplemented!()
+    }
+}
