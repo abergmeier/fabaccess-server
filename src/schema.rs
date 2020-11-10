743de370ab3ba9078da54f83c45238025432e319
Gregor Reitzenstein <me@dequbed.space>
2020-11-10 13:37:07 +0100

    Silences the dead cap'n proto code warnings

--- src/schema.rs
+++ src/schema.rs
@@ -1,11 +1,14 @@
+#[allow(dead_code)]
 pub mod auth_capnp {
     include!(concat!(env!("OUT_DIR"), "/schema/auth_capnp.rs"));
 }
 
+#[allow(dead_code)]
 pub mod api_capnp {
     include!(concat!(env!("OUT_DIR"), "/schema/api_capnp.rs"));
 }
 
+#[allow(dead_code)]
 pub mod connection_capnp {
     include!(concat!(env!("OUT_DIR"), "/schema/connection_capnp.rs"));
 }
