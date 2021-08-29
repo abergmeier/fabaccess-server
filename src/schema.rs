8e1c0ea0a397438f2f0c5a61f9fc2858c0038cfc
Gregor Reitzenstein <me@dequbed.space>
2021-08-29 11:10:24 +0200

    Setup new schema modules

--- src/schema.rs
+++ src/schema.rs
@@ -1,14 +1,59 @@
 #[allow(dead_code)]
-pub mod auth_capnp {
-    include!(concat!(env!("OUT_DIR"), "/schema/auth_capnp.rs"));
-}
-
-#[allow(dead_code)]
-pub mod api_capnp {
-    include!(concat!(env!("OUT_DIR"), "/schema/api_capnp.rs"));
+pub mod authenticationsystem_capnp {
+    include!(concat!(env!("OUT_DIR"), "/schema/authenticationsystem_capnp.rs"));
 }
 
 #[allow(dead_code)]
 pub mod connection_capnp {
     include!(concat!(env!("OUT_DIR"), "/schema/connection_capnp.rs"));
+}
+
+#[allow(dead_code)]
+pub mod federationsystem_capnp {
+    include!(concat!(env!("OUT_DIR"), "/schema/federationsystem_capnp.rs"));
+}
+
+#[allow(dead_code)]
+pub mod general_capnp {
+    include!(concat!(env!("OUT_DIR"), "/schema/general_capnp.rs"));
+}
+
+#[allow(dead_code)]
+pub mod interactionsystem_capnp {
+    include!(concat!(env!("OUT_DIR"), "/schema/interactionsystem_capnp.rs"));
+}
+
+#[allow(dead_code)]
+pub mod machine_capnp {
+    include!(concat!(env!("OUT_DIR"), "/schema/machine_capnp.rs"));
+}
+
+#[allow(dead_code)]
+pub mod machinesystem_capnp {
+    include!(concat!(env!("OUT_DIR"), "/schema/machinesystem_capnp.rs"));
+}
+
+#[allow(dead_code)]
+pub mod permissionsystem_capnp {
+    include!(concat!(env!("OUT_DIR"), "/schema/permissionsystem_capnp.rs"));
+}
+
+#[allow(dead_code)]
+pub mod role_capnp {
+    include!(concat!(env!("OUT_DIR"), "/schema/role_capnp.rs"));
+}
+
+#[allow(dead_code)]
+pub mod space_capnp {
+    include!(concat!(env!("OUT_DIR"), "/schema/space_capnp.rs"));
+}
+
+#[allow(dead_code)]
+pub mod user_capnp {
+    include!(concat!(env!("OUT_DIR"), "/schema/user_capnp.rs"));
+}
+
+#[allow(dead_code)]
+pub mod usersystem_capnp {
+    include!(concat!(env!("OUT_DIR"), "/schema/usersystem_capnp.rs"));
 }
