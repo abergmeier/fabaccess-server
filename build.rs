678a274544b2d1a8d62b4e4bca533c93b7c65026
Gregor Reitzenstein <me@dequbed.space>
2021-08-29 10:09:41 +0200

    Make the build script smarter

--- build.rs
+++ build.rs
@@ -1,5 +1,39 @@
+use walkdir::{WalkDir, DirEntry};
+
+fn is_hidden(entry: &DirEntry) -> bool {
+    entry.file_name()
+        .to_str()
+        .map(|s| s.starts_with("."))
+        .unwrap_or(false)
+}
+
 fn main() {
-    ::capnpc::CompilerCommand::new().file("schema/connection.capnp").run().unwrap();
-    ::capnpc::CompilerCommand::new().file("schema/api.capnp").run().unwrap();
-    ::capnpc::CompilerCommand::new().file("schema/auth.capnp").run().unwrap();
+    // Tell cargo to only run this script if the schema files or this script have changed
+    println!("cargo:rerun-if-changed=schema");
+
+    let mut compile_command = ::capnpc::CompilerCommand::new();
+
+    // Set parent module of all generated schema files.
+    // i.e. a file "user.capnp" will result in module "schema::user"
+    compile_command.default_parent_module(vec!["schema".into()]);
+
+    for entry in WalkDir::new("schema")
+        .max_depth(2)
+        .into_iter()
+        .filter_entry(|e| !is_hidden(e))
+        .filter_map(Result::ok) // Filter all entries that access failed on
+        .filter(|e| !e.file_type().is_dir()) // Filter directories
+        // Filter non-schema files
+        .filter(|e| e.file_name()
+                     .to_str()
+                     .map(|s| s.ends_with(".capnp"))
+                     .unwrap_or(false)
+        )
+    {
+        println!("Collecting schema file {}", entry.path().display());
+        compile_command.file(entry.path());
+    }
+
+    println!("Compiling schemas...");
+    compile_command.run().expect("Failed to generate API code");
 }
