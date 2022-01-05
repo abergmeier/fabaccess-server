03ff3fcf86dffdf5299f9577003421927ac2b736
Nadja Reitzenstein <me@dequbed.space>
2022-01-05 21:56:32 +0100

    Modules state commit

---
+++ modules/dummy/src/lib.rs
@@ -0,0 +1,29 @@
+use sdk::BoxFuture;
+use sdk::initiators::{Initiator, InitiatorError, ResourceID, UpdateSink};
+
+#[sdk::module]
+struct Dummy {
+    a: u32,
+    b: u32,
+    c: u32,
+    d: u32,
+}
+
+impl Initiator for Dummy {
+    fn start_for(&mut self, machine: ResourceID) -> BoxFuture<'static, Result<(), Box<dyn InitiatorError>>> {
+        todo!()
+    }
+
+    fn run(&mut self, request: &mut UpdateSink) -> BoxFuture<'static, Result<(), Box<dyn InitiatorError>>> {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
