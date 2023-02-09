98c2e3fd017459bca675cb8515bc67e5e787dda5
Nadja Reitzenstein <me@dequbed.space>
2023-02-09 17:07:22 +0100

    rsasl update

--- bffhd/authentication/fabfire/mod.rs
+++ bffhd/authentication/fabfire/mod.rs
@@ -2,13 +2,27 @@ mod server;
 pub use server::FabFire;
 
 use rsasl::mechname::Mechname;
-use rsasl::registry::{Mechanism, Side, MECHANISMS};
+use rsasl::registry::{Matches, Mechanism, Named, Side, MECHANISMS};
 
 const MECHNAME: &'static Mechname = &Mechname::const_new_unchecked(b"X-FABFIRE");
 
 #[linkme::distributed_slice(MECHANISMS)]
-pub static FABFIRE: Mechanism =
-    Mechanism::build(MECHNAME, 300, None, Some(FabFire::new_server), Side::Client);
+pub static FABFIRE: Mechanism = Mechanism::build(
+    MECHNAME,
+    300,
+    None,
+    Some(FabFire::new_server),
+    Side::Client,
+    |_| Some(Matches::<Select>::name()),
+    |_| true,
+);
+
+struct Select;
+impl Named for Select {
+    fn mech() -> &'static Mechanism {
+        &FABFIRE
+    }
+}
 
 use rsasl::property::SizedProperty;
 use std::marker::PhantomData;
--- bffhd/authentication/fabfire_bin/mod.rs
+++ bffhd/authentication/fabfire_bin/mod.rs
@@ -2,10 +2,24 @@ mod server;
 pub use server::FabFire;
 
 use rsasl::mechname::Mechname;
-use rsasl::registry::{Mechanism, Side, MECHANISMS};
+use rsasl::registry::{Matches, Mechanism, Named, Side, MECHANISMS};
 
 const MECHNAME: &'static Mechname = &Mechname::const_new_unchecked(b"X-FABFIRE-BIN");
 
 #[linkme::distributed_slice(MECHANISMS)]
-pub static FABFIRE: Mechanism =
-    Mechanism::build(MECHNAME, 300, None, Some(FabFire::new_server), Side::Client);
+pub static FABFIRE: Mechanism = Mechanism::build(
+    MECHNAME,
+    300,
+    None,
+    Some(FabFire::new_server),
+    Side::Client,
+    |_| Some(Matches::<Select>::name()),
+    |_| true,
+);
+
+struct Select;
+impl Named for Select {
+    fn mech() -> &'static Mechanism {
+        &FABFIRE
+    }
+}
