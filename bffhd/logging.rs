06083a63e35875d6a19db2a83b35df1565be5c3d
= <=>
2024-12-15 22:29:14 +0100

    Fix compilation error caused by overlooked renames: diflouroborane -> difluoroborane

--- bffhd/logging.rs
+++ bffhd/logging.rs
@@ -9,8 +9,8 @@ pub struct LogConfig {
     #[serde(default, skip_serializing_if = "Option::is_none")]
     /// Log filter string in the tracing format `target[span{field=value}]=level`.
     /// lvalue is optional and multiple filters can be combined with comma.
-    /// e.g. `warn,diflouroborane::actors=debug` will only print `WARN` and `ERROR` unless the
-    /// message is logged in a span below `diflouroborane::actors` (i.e. by an actor task) in
+    /// e.g. `warn,difluoroborane::actors=debug` will only print `WARN` and `ERROR` unless the
+    /// message is logged in a span below `difluoroborane::actors` (i.e. by an actor task) in
     /// which case `DEBUG` and `INFO` will also be printed.
     pub filter: Option<String>,
 
